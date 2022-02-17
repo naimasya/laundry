35f88c140b4bb72b4b9f5f245a43b2d58f431f42
naimasya <99864675+naimasya@users.noreply.github.com>
2022-02-17 20:42:13 +0700

    Create hapusOutlet.php

---
+++ outlet/hapusOutlet.php
@@ -0,0 +1,12 @@
+<?php
+if($_GET['id']){
+    include "../koneksi.php";
+    $qry_hapus = mysqli_query($conn,"delete from outlet where id='".$_GET['id']."'");
+
+    if ($qry_hapus) {
+        echo "<script>alert('Sukses hapus outlet');location.href='tampilOutlet.php';</script>";
+    } else {
+    echo "<script>alert('Gagal hapus outlet');location.href='tampilOutlet.php';</script>";
+    }
+  }
+?>
